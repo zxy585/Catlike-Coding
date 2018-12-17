d29ebd486763303efca364d53fcd6b469f3d0f82
张鑫焱 <444764743@qq.com>
2018-12-17 13:27:12 +0800

    添加Screen Effects相关代码，以及示例场景

---
+++ Catlike Coding/Assets/Shader Coding/Screen Effects/TextureAdjustEffect.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//继承自PostEffectBase
+public class TextureAdjustEffect : PostEffectBase
+{
+    //需要使用的图像
+    public List<Texture2D> blendTexture = new List<Texture2D>();
+    public int framenum = 1;
+    private int Texturenum = 0;
+    private int num = 0;
+    private int frame = 0;
+    //通过Range控制可以输入的参数的范围
+    [Range(0.0f, 3.0f)]
+    public float brightness = 1.0f;//亮度
+    [Range(0.0f, 1.0f)]
+    public float opacity = 1.0f;//混合程度
+    //覆写OnRenderImage函数
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        //仅仅当有材质的时候才进行后处理，如果_Material为空，不进行后处理
+        if (_Material)
+        {
+            //通过Material.SetXXX（"name",value）可以设置shader中的参数值
+            _Material.SetFloat("_Brightness", brightness);
+            _Material.SetTexture("_BlendTex", blendTexture[num]);
+            _Material.SetFloat("_Opacity", opacity);
+            //使用Material处理Texture，dest不一定是屏幕，后处理效果可以叠加的！
+            Graphics.Blit(src, dest, _Material);
+        }
+        else
+        {
+            //直接绘制
+            Graphics.Blit(src, dest);
+        }
+    }
+
+    private void Start()
+    {
+        opacity = 0;
+        num = 0;
+        Texturenum = blendTexture.Count - 1;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isPlay)
+        {
+            opacity = 0;
+            num = 0;
+            brightness = 1;
+            return;
+        }
+        if (brightness > 0.6)
+        {
+            brightness -= 0.05f;
+            return;
+        }
+        opacity = 1;
+        if (num >= Texturenum) num = 20;
+
+        frame += 1;
+        if (frame >= framenum)
+        {
+            frame = 0;
+            num += 1;
+        }
+    }
+
+
+}
---
+++ Catlike Coding/Assets/Shader Coding/Screen Effects/TextureAdjustEffect.shader
@@ -0,0 +1,55 @@
+﻿Shader "PostEffect/TextureBlendEffect"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_Brightness("Brightness", Range(0,3)) = 1
+		_BlendTex("Blend Texture", 2D) = "white"{}
+		_Opacity("Blend Opacity", Range(0,1)) = 1
+	}
+
+		SubShader
+		{
+
+			Pass
+		  {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#pragma fragmentoption ARB_precision_hint_fastest
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			uniform sampler2D _BlendTex;
+			fixed _Opacity;
+			half _Brightness;
+
+			fixed4 frag(v2f_img i) : COLOR
+			{
+				//Get the colors from the RenderTexture and the uv's
+				//from the v2f_img struct
+				fixed4 renderTex = tex2D(_MainTex, i.uv);
+				fixed4 blendTex = tex2D(_BlendTex, i.uv);
+				fixed3 finalColor = renderTex * _Brightness;
+				renderTex = fixed4(finalColor, renderTex.a);
+
+				//Perform a multiply Blend mode
+                //Multiply混合模式
+				fixed4 blendedMultiply = renderTex * blendTex;
+                //Add Blend模式
+				fixed4 blendedAdd = renderTex + blendTex;
+                //Overlay混合模式
+				fixed4 blendedScreen = (1-(1-renderTex)*(1-blendTex));
+
+				//Adjust amount of Blend Mode with a lerp
+				renderTex = lerp(renderTex, blendedScreen, _Opacity);
+
+				return renderTex;
+			}
+
+			ENDCG
+		}
+
+	}
+	FallBack off
+}
