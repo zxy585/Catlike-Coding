2650467caf762015363af6d2fa9886a8c19e3746
张鑫焱 <444764743@qq.com>
2019-01-07 09:40:22 +0800

    1.7提交

---
+++ Catlike Coding/Assets/Z_Unity/Collection/KeyCodeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyCodeTest : MonoBehaviour
+{
+    public Text t01;
+
+    void OnGUI()
+    {
+        //if (Input.anyKeyDown) 
+        //{
+        //    e = Event.KeyboardEvent;
+        //    if (e.isKey)
+        //    {
+        //        currentKey = e.keyCode;
+        //        Debug.LogError("Current Key is : " + currentKey.ToString());
+        //    }
+        //}
+
+        //循环遍历输出
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    Debug.LogError("Current Key is : " + keyCode.ToString());
+                    t01.text = "Current Key is : " + keyCode.ToString();
+                }
+            }
+        }
+    }
+}
